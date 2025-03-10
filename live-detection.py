6c59ef814e7551d74df5ca30729d1fa33e7969e8
Miracle Sitompul <rimaputri9879@gmail.com>
2025-03-10 23:05:09 +0800

    Menggabungkan dua model dan fitur live beserta upload test foto

---
+++ live-detection.py
@@ -0,0 +1,26 @@
+from ultralytics import YOLO
+import cv2
+model_api_kecil = YOLO('api_kecil_model.pt')
+model_api_besar = YOLO('api_besar_model.pt')
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    results = model_api_kecil(frame, imgsz=640, conf=0.5)
+    if any(len(result.boxes) > 0 for result in results):
+        label = "API KECIL"
+    else:
+        results = model_api_besar(frame, imgsz=640, conf=0.5)
+        label = "API BESAR" if any(len(result.boxes) > 0 for result in results) else "TIDAK ADA API"
+    for result in results:
+        for box in result.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])  
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)  
+            cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    cv2.imshow("Campus Fire Guard", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 from ultralytics import YOLO
+import os
 
-model = YOLO('best.pt')
-model.predict(source = 0, imgsz=640, conf=0.6, show=True)
+model_api_kecil = YOLO('api_kecil_model.pt')
+model_api_besar = YOLO('api_besar_model.pt')
+
+
+image_path = "FirePhotography.jpg"
+
+
+output_dir = "runs/detect/"
+os.makedirs(output_dir, exist_ok=True)
+
+
+results_api_kecil = model_api_kecil.predict(source=image_path, imgsz=640, conf=0.5)
+
+if any(len(result.boxes) > 0 for result in results_api_kecil):
+    print("Terdeteksi api kecil, menyimpan hasil dari model api kecil.")
+    model_api_kecil.predict(source=image_path, imgsz=640, conf=0.5, save=True)
+else:
+    print("Tidak ada api kecil, menjalankan model api besar.")
+    model_api_besar.predict(source=image_path, imgsz=640, conf=0.5, save=True)
+
+print(f"Gambar hasil prediksi disimpan di folder: {output_dir}")
