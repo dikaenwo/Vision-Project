5b2a9249c0972da23fbcd457a871014e51c2d82b
Miracle Sitompul <rimaputri9879@gmail.com>
2025-03-14 12:24:52 +0800

    mengubah model baru

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from ultralytics import YOLO
 import os
 
-model_api_kecil = YOLO('best_api_kecil.pt')
+model_api_kecil = YOLO('lilin.pt')
 model_api_besar = YOLO('api_besar_model.pt')
 
 
-image_path = "FirePhotography.jpg"
+image_path = "nr2.jpg"
 
 
 output_dir = "runs/detect/"
 os.makedirs(output_dir, exist_ok=True)
 
 
-results_api_kecil = model_api_kecil.predict(source=image_path, imgsz=640, conf=0.5)
+results_api_kecil = model_api_kecil.predict(source=image_path, imgsz=640, conf=0.3)
 
 if any(len(result.boxes) > 0 for result in results_api_kecil):
     print("Terdeteksi api kecil, menyimpan hasil dari model api kecil.")
-    model_api_kecil.predict(source=image_path, imgsz=640, conf=0.5, save=True)
+    model_api_kecil.predict(source=image_path, imgsz=640, conf=0.3, save=True)
 else:
     print("Tidak ada api kecil, menjalankan model api besar.")
-    model_api_besar.predict(source=image_path, imgsz=640, conf=0.5, save=True)
+    model_api_besar.predict(source=image_path, imgsz=640, conf=0.3, save=True)
 
 print(f"Gambar hasil prediksi disimpan di folder: {output_dir}")
